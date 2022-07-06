1716b9e535932a4892e31b5f0a43503886561580
Lalaine <lsmarasigan15@gmail.com>
2022-07-06 19:08:55 +0800

    Add update and edit functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,11 @@ class ArticlesController < ApplicationController
 
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -24,6 +29,17 @@ class ArticlesController < ApplicationController
   end
 
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
+
 
 
