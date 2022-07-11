fd3b7351fd61a0e5a706dc35653d2deac078e75d
Lalaine <lsmarasigan15@gmail.com>
2022-07-11 17:13:18 +0800

    DRY edits

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [ :edit, :update, :destroy]
 
   def show
     @article = Article.where(id: params[:id])
-  
-    if not @article.present?
+      if not @article.present?
       redirect_to articles_path 
     else
       @article = @article.first
     end
-
-
   end
 
   def index
@@ -18,15 +16,13 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
-
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -36,8 +32,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -46,10 +41,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
 end
 
