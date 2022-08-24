14366a1113abe552fae1e3baeaa13ea9a631cb0a
Lalaine <lsmarasigan15@gmail.com>
2022-08-24 19:57:22 +0800

    Add user functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the AlphaBlog, you have successfully signed up!"
+      flash[:notice] = "Welcome to the AlphaBlog, #{@user.username} you have successfully signed up!"
       redirect_to articles_path
     else
       render 'new'
